25078f25e360645d22eae0a859353e05b44ea5d2
ARGithubNiNJA <arshkumar930@gmail.com>
2026-06-23 00:51:00 +0530

    Initial commit

---
+++ Day 9/DictionaryExample.py
@@ -0,0 +1,12 @@
+#Dictionaries{key, value}
+programming_language={
+    "name": "Arsh",
+    "age": 17,
+    "course": "Python",
+    "city": "Delhi",
+}
+
+print(programming_language)
+print(programming_language["name"])
+
+#Assiginng the value
