ee245a89b956a42c2dd9155253b93a70929123e9
ARGithubNiNJA <arshkumar930@gmail.com>
2026-06-24 06:48:04 +0530

    Dictionary basic example

--- Day 9/DictionaryExample.py
+++ Day 9/DictionaryExample.py
@@ -5,8 +5,22 @@ programming_language={
     "course": "Python",
     "city": "Delhi",
 }
-
+#printing dictionaray
 print(programming_language)
+#printing a specific value using key
 print(programming_language["name"])
 
+
+#inserting a value
+programming_language["job"] = "Software Engineer"
+print(programming_language)
+
+#Looping over the dictionary
+for key in programming_language:
+    print(key)
+    print(programming_language[key])
+
+#emptying a dictionary
+programming_language={}
+print(programming_language)
 #Assiginng the value
