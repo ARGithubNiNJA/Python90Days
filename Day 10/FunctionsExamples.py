580577791f6d448a8e5e58b7328da01324fdc140
ARGithubNiNJA <arshkumar930@gmail.com>
2026-06-25 19:57:48 +0530

    Simple Function example calculater

---
+++ Day 10/FunctionsExamples.py
@@ -0,0 +1,20 @@
+##  Functions examples
+
+def sum(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if b==0:
+        return "Infinite"
+    else:
+        return a/b
+
+num1=input("Enter the first number\n")
+operation=input("Enter the operation\n")
+num2=input("Enter the second number\n")
+
+
+
