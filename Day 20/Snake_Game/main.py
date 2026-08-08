1e42469d98a06679e13b4fc6c1c23490d6c99910
ARGithubNiNJA <arshkumar930@gmail.com>
2026-08-08 15:13:37 +0530

    files replaced on Snake folder

--- Day 20/Snake_Game/main.py
+++ Day 20/Snake_Game/main.py
@@ -1,48 +1,50 @@
 from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
-from Snake import Snake
-from Food import  Food
-from Scoreboard import  Scoreboard
 
 screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.setup(width=600,height=600)
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
-#todo step 1 create a initial snake body
-# turtle_1=Turtle()
-# turtle_1.shape("square")
-# turtle_1.color("white")
-#
-# turtle_2=Turtle()
-# turtle_2.shape("square")
-# turtle_2.color("white")
-# turtle_1.goto(-20,0)
-#
-# turtle_3=Turtle("square")
-# turtle_3.color("white")
-# turtle_3.goto(-40,0)
 
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-#todo Moving the turtle in sync
 
-game_on=True
-while game_on:
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    #detect the collision
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
-        scoreboard.increase_score()
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 
