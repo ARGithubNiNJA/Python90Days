fc64dd57e7d6ea4b98d4c68a488737d61a3200de
ARGithubNiNJA <arshkumar930@gmail.com>
2026-08-17 23:19:32 +0530

    Day 28 Pomodora app added

---
+++ Day 28/main.py
@@ -0,0 +1,78 @@
+from itertools import count
+import math
+import re
+from tkinter import *
+from math import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps=0
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps+=1
+    work_sec=WORK_MIN*60
+    short_break_sec=SHORT_BREAK_MIN*60
+    long_break_sec=LONG_BREAK_MIN*60
+    count_down(work_sec)
+    if reps%8==0:
+        count_down(long_break_sec)
+    elif reps%2==0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min=math.floor(count/60)
+    count_sec=count%60
+    if count_sec==0:
+        count_sec="00"
+    elif count_sec<10 :
+        count_sec=f"0{count_sec}"
+    else:
+        start_timer()
+ 
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count>0:
+        window.after(1000,count_down,count-1)
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodora")
+window.config(padx=100,pady=50,bg=YELLOW)
+
+tomato_label=Label(text="Timer",bg=YELLOW,fg=GREEN,font=(FONT_NAME,50))
+tomato_label.grid(column=1,row=0)
+
+canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+tomato_png = PhotoImage(file="D://Arsh//pypy//Pomodora App Angela YU/tomato.png")
+canvas.create_image(100, 112, image=tomato_png)
+timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.grid(column= 1, row= 1)
+
+# count_down(5)
+
+button_start=Button(text="Start",highlightthickness=0,command=start_timer)
+button_start.grid(column= 0,row=2)
+
+button_end=Button(text="Reset",highlightthickness=0)
+button_end.grid(column= 2,row=2)
+
+Check_label=Label(text="✔",bg=YELLOW,fg=GREEN,font=(FONT_NAME))
+Check_label.grid(column=1,row=2)
+
+# canvas.pack()
+
+window.mainloop()
+
+
+window.mainloop()
